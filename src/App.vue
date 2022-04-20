283ab944e0b28f645715e0d9a0254274c4d30fe8
Roger J. A. Duthie <343584+rjaduthie@users.noreply.github.com>
2022-04-20 13:53:29 +0100

    Initial map with hard-coded data

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <BangleMap />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import BangleMap from './components/BangleMap.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    BangleMap
   }
 }
 </script>
