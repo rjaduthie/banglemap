48ae1b1c5ea78b4f1bf14d19fa38d18c3f775140
Roger J. A. Duthie <343584+rjaduthie@users.noreply.github.com>
2022-05-15 21:09:28 +0100

    File selector for route.

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,37 @@
 <template>
-  <BangleMap />
+  <div>
+    <p>My File Selector:
+      <bangle-run-data-selector v-model="file"
+                                @change="fileSelected($event)"></bangle-run-data-selector>
+    </p>
+    <p v-if="file">{{ file.name }}</p>
+    <p v-else>No file selected.</p>
+  </div>
+  <BangleMap v-model:run-data="runData"/>
 </template>
 
 <script>
 import BangleMap from './components/BangleMap.vue'
+import BangleRunDataSelector from "@/components/BangleRunDataSelector";
+import Papa from "papaparse";
 
 export default {
   name: 'App',
   components: {
-    BangleMap
+    BangleMap,
+    BangleRunDataSelector
+  },
+  data() {
+    return {file: null, runData: null}
+  },
+  methods: {
+    fileSelected: function (event) {
+      Papa.parse(event.target.files[0], {
+        header: true, complete: (results) => {
+          this.runData = results.data
+        }
+      })
+    }
   }
 }
 </script>
